fd1c34e67f59c9f2425232053dece4161615eec7
Joakim Wassberg <joakim@arthead.se>
2018-11-12 10:38:32 +0100

    Initial commit

---
+++ MessageService/IMessageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Net.Security;
+
+namespace MessageService
+{
+    [ServiceContract]
+    public interface IMessageService
+    {
+        [OperationContract]
+        string GetMessage(string name);
+
+        [OperationContract(ProtectionLevel = ProtectionLevel.None)]
+        string GetMessageWithoutAnyProtection();
+
+        [OperationContract(ProtectionLevel = ProtectionLevel.Sign)]
+        string GetSignedMessage();
+
+        [OperationContract(ProtectionLevel = ProtectionLevel.EncryptAndSign)]
+        string GetSignedAndEncryptedMessage();
+    }
+}
---
+++ MessageService/MessageService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessageService
+{
+    public class MessageService : IMessageService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public string GetMessageWithoutAnyProtection()
+        {
+            return "This message is sent without any protection";
+        }
+
+        public string GetSignedAndEncryptedMessage()
+        {
+            return "This is a signed and encrypted message";
+        }
+
+        public string GetSignedMessage()
+        {
+            return "This is a signed message";
+        }
+    }
+}
